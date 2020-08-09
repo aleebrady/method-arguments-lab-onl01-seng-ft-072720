8af06f91d1d83a4a431623ca781710e11c63d833
Aaron Brady <aleebrady@gmail.com>
2020-08-09 21:29:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
@@ -5,3 +6,6 @@ end
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+=======
+def introduction 
+>>>>>>> 6e1f24132c5a3f52aba789eb49cbf2fdc04b7526
